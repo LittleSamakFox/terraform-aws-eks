4a87feb923449779503d3d9a498a9542ff3ce5c1
littlesamakfox <securityofanime@gmail.com>
2022-04-10 03:34:08 +0900

    ACL 규칙 변경, 오류 수정

--- acl.tf
+++ acl.tf
@@ -22,151 +22,148 @@ resource "aws_default_network_acl" "k5s_vpc_acl_default" {
 
 #퍼블릭 서브넷에서 사용할 네트워크 ACL 생성
 resource "aws_network_acl" "k5s_acl_public" {
-    vpc_id = aws_vpc.k5s_vpc.id
-    subnet_ids = [
-      aws_subnet.k5s_public_subnet.0.id,
-      aws_subnet.k5s_public_subnet.1.id,
+  vpc_id = aws_vpc.k5s_vpc.id
+  subnet_ids = [
+    aws_subnet.k5s_public_subnet[0].id,
+    aws_subnet.k5s_public_subnet[1].id,
+    aws_subnet.k5s_public_subnet[2].id,
     ]
+  ingress { //HTTP
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    rule_no = 100
+  }
+  ingress { //HTTPS
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    rule_no = 110
+  }
+  ingress { //SSH
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    rule_no = 120
+  }
+  ingress { //RDP
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 3389
+    to_port = 3389
+    protocol = "tcp"
+    rule_no = 130
+  }
+  ingress { //CustomTCP
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 1024
+    to_port = 65535
+    protocol = "tcp"
+    rule_no = 140
+  }
+  ingress { //ICMP
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 0
+    to_port = 0
+    protocol = "icmp"
+    rule_no = 1
+  }
+  egress { //HTTP
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    rule_no = 100
+  }
+  egress { //HTTPS
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    rule_no = 110
+  }
+  egress { //SSH
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 1024
+    to_port = 65535
+    protocol = "tcp"
+    rule_no = 120
+  }
+  egress { //CustomTCP
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 1024
+    to_port = 65535
+    protocol = "tcp"
+    rule_no = 140
+  }
+  egress {
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+    protocol   = -1
+    rule_no    = 2
+  }
 }
-#퍼블릭 서브넷 용 네트워크 ACL 규칙 추가
-resource "aws_network_acl_rule" "k5s_acl_public_http_ingress" {
-  network_acl_id = aws_network_acl.k5s_acl_public.id
-  rule_number = 100
-  rule_action = "allow"
-  egress = false
-  protocol = "tcp"
-  cidr_block = "0.0.0.0/0"
-  from_port = 80
-  to_port = 80
-}
-resource "aws_network_acl_rule" "k5s_acl_public_http_egress" {
-  network_acl_id = aws_network_acl.k5s_acl_public.id
-  rule_number = 100
-  rule_action = "allow"
-  egress = true
-  protocol = "tcp"
-  cidr_block = "0.0.0.0/0"
-  from_port = 80
-  to_port = 80
-}
-resource "aws_network_acl_rule" "k5s_acl_public_https_ingress" {
-  network_acl_id = aws_network_acl.k5s_acl_public.id
-  rule_number = 110
-  rule_action = "allow"
-  egress = false
-  protocol = "tcp"
-  cidr_block = "0.0.0.0/0"
-  from_port = 443
-  to_port = 443
-}
-resource "aws_network_acl_rule" "k5s_acl_public_https_egress" {
-  network_acl_id = aws_network_acl.k5s_acl_public.id
-  rule_number = 110
-  rule_action = "allow"
-  egress = true
-  protocol = "tcp"
-  cidr_block = "0.0.0.0/0"
-  from_port = 443
-  to_port = 443
-}
-resource "aws_network_acl_rule" "k5s_acl_public_ssh_ingress" {
-  network_acl_id = aws_network_acl.k5s_acl_public.id
-  rule_number = 120
-  rule_action = "allow"
-  egress = false
-  protocol = "tcp"
-  cidr_block = "0.0.0.0/0"
-  from_port = 22
-  to_port = 22
-}
-resource "aws_network_acl_rule" "k5s_acl_public_ssh_egress" {
-  network_acl_id = aws_network_acl.k5s_acl_public.id
-  rule_number = 120
-  rule_action = "allow"
-  egress = true
-  protocol = "tcp"
-  cidr_block = aws_vpc.k5s_vpc.cidr_block //cidr블록으로 설정
-  from_port = 22
-  to_port = 22
-}
-resource "aws_network_acl_rule" "k5s_acl_public_customtcp_ingress" {
-  network_acl_id = aws_network_acl.k5s_acl_public.id
-  rule_number = 140
-  rule_action = "allow"
-  egress = false
-  protocol = "tcp"
-  cidr_block = "0.0.0.0/0"
-  from_port = 1024 //NAT게이트웨이 포트는 1024부터 사용
-  to_port = 65535
-}
-resource "aws_network_acl_rule" "k5s_acl_public_customtcp_egress" {
-  network_acl_id = aws_network_acl.k5s_acl_public.id
-  rule_number = 140
-  rule_action = "allow"
-  egress = true
-  protocol = "tcp"
-  cidr_block = "0.0.0.0/0"
-  from_port = 1024
-  to_port = 65535
-}
-
 
 #프라이빗 서브넷에서 사용할 ACL 규칙 추가
 resource "aws_network_acl" "k5s_acl_private" {
     vpc_id = aws_vpc.k5s_vpc.id
     subnet_ids = [
-      aws_subnet.k5s_private_subnet.0.id,
-      aws_subnet.k5s_private_subnet.1.id,
+      aws_subnet.k5s_private_subnet[0].id,
+      aws_subnet.k5s_private_subnet[1].id,
+      aws_subnet.k5s_private_subnet[2].id,
     ]
-}
-#프라이빗 서브넷 용 네트워크 ACL 규칙 추가
-resource "aws_network_acl_rule" "k5s_acl_private_ingress" {
-    network_acl_id = aws_network_acl.k5s_acl_private.id
-    rule_number = 100
-    rule_action = "allow"
-    egress = false
-    protocol = -1
-    cidr_block = aws_vpc.k5s_vpc.cidr_block
-    from_port = 0
-    to_port = 0
-}
-resource "aws_network_acl_rule" "k5s_acl_private_egress" {
-    network_acl_id = aws_network_acl.k5s_acl_private.id
-    rule_number = 100
-    rule_action = "allow"
-    egress = true
-    protocol = -1
-    cidr_block = aws_vpc.k5s_vpc.cidr_block
-    from_port = 0
-    to_port = 0
-}
-resource "aws_network_acl_rule" "k5s_acl_private_nat_ingress" {
-  network_acl_id = aws_network_acl.k5s_acl_private.id
-  rule_number = 110
-  rule_action = "allow"
-  egress = false
-  protocol = "tcp"
-  cidr_block = "0.0.0.0/0"
-  from_port = 1024
-  to_port = 65535
-}
-resource "aws_network_acl_rule" "k5s_acl_private_http_egress" {
-    network_acl_id = aws_network_acl.k5s_acl_private.id
-    rule_number = 120
-    rule_action = "allow"
-    egress = true
-    protocol = "tcp"
-    cidr_block = aws_vpc.k5s_vpc.cidr_block
-    from_port = 80
-    to_port = 80
-}
-resource "aws_network_acl_rule" "k5s_acl_private_https_egress" {
-  network_acl_id = aws_network_acl.k5s_acl_private.id
-  rule_number = 130
-  rule_action = "allow"
-  egress = true
-  protocol = "tcp"
-  cidr_block = "0.0.0.0/0"
-  from_port = 443
-  to_port = 443
+    ingress {//NAT Ingress
+      action = "allow"
+      cidr_block = "0.0.0.0/0"
+      from_port = 1024
+      to_port = 65535
+      protocol = "tcp"
+      rule_no = 140
+    }
+    ingress {
+      action = "allow"
+      cidr_block = aws_vpc.k5s_vpc.cidr_block
+      from_port = 0
+      to_port = 0
+      protocol = -1
+      rule_no = 100
+    }
+    egress { //http
+      action = "allow"
+      cidr_block = "0.0.0.0/0"
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      rule_no = 100
+    }
+    egress { //https
+      action = "allow"
+      cidr_block = "0.0.0.0/0"
+      from_port = 443
+      to_port = 443
+      protocol = "tcp"
+      rule_no = 110
+    }
+    egress {
+      action = "allow"
+      cidr_block = aws_vpc.k5s_vpc.cidr_block
+      from_port = 0
+      to_port = 0
+      protocol = -1
+      rule_no = 2
+    }
 }
