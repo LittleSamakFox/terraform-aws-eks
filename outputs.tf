ca293b810a9c77b1d1005bd5ee151552129d7a79
littlesamakfox <securityofanime@gmail.com>
2022-04-15 18:52:18 +0900

    bastion ip, cluster arn 출력 시키도록 output 변경

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,17 @@ output "cluster_endpoint" {
   value = aws_eks_cluster.k5s_cluster.endpoint
 }
 
+output "bastion_ip" {
+  value = aws_eip.k5s_bastion_eip.public_ip
+  description = "bastion ec2 EIP, u can access it ssh -i ~/.ssh/[PRIVATE_KEY이름].pem ec2-user@[public ip주소]"
+}
+
+/*
 output "cluster_ca_certificate" {
   value = aws_eks_cluster.k5s_cluster.certificate_authority[0].data
+}
+*/
+
+output "cluster_arn" {
+  value = aws_eks_cluster.k5s_cluster.arn
 }
